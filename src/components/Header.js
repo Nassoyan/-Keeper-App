cf68fd41863316c4f11fe235659ca1bb3bf4dce3
Tigran <112520303+Nassoyan@users.noreply.github.com>
2023-05-23 15:32:00 +0400

    added surname on the right side of avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,14 @@ function Header() {
       <h1>
         <HighlightIcon />
         Keeper
+      </h1>
+      <div className="avatar">
         <Avatar
           alt="Tigran"
           src="https://i.pinimg.com/280x280_RS/da/bc/58/dabc58c3c5817afef363c83c29c8f6ee.jpg"
         />
-      </h1>
+        <i className="nasoyan">Nasoyan</i>
+      </div>
     </header>
   );
 }
