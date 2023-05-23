5d710300d400d42f89b9d9ec1c1a518d4b7da26a
Tigran <112520303+Nassoyan@users.noreply.github.com>
2023-05-23 12:40:31 +0400

    avatar img added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HighlightIcon from "@mui/icons-material/Highlight";
+import Avatar from "@material-ui/core/Avatar";
 
 function Header() {
   return (
@@ -7,6 +8,10 @@ function Header() {
       <h1>
         <HighlightIcon />
         Keeper
+        <Avatar
+          alt="Tigran"
+          src="https://i.pinimg.com/280x280_RS/da/bc/58/dabc58c3c5817afef363c83c29c8f6ee.jpg"
+        />
       </h1>
     </header>
   );
